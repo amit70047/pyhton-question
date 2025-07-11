d73fc26b4f04e69fe06c7882e606b1400166a52c
Amit kumar <214317715+amit70047@users.noreply.github.com>
2025-07-11 11:02:20 +0530

    Add files via upload

---
+++ Creating a Custom Module/Task 1/math_utils.py
@@ -0,0 +1,23 @@
+
+a = 10
+b = 5
+
+# Addition
+add = a + b
+
+# Subtraction
+subtract = a - b
+
+# Multiplication
+multiply = a * b
+
+# Division 
+if b == 0:
+    divide = "Error: Cannot divide by zero"
+else:
+    divide = a / b
+
+print("Addition:", add)
+print("Subtraction:", subtract)
+print("Multiplication:", multiply)
+print("Division:", divide)
