c9a99b4b72e0f24acfe4c2e4a4c23909f1959027
Amit kumar <214317715+amit70047@users.noreply.github.com>
2025-07-11 08:43:20 +0530

    Delete Question4.py

--- Question4.py
+++
@@ -1,9 +0,0 @@
-# Q4. Write a function factorial(n) that returns the factorial of a number using a loop.
-
-n = int(input("Enter a number: "))
-
-factorial = 1
-for i in range(1, n + 1):
-    factorial *= i
-
-print(f"Factorial of {n} is: ")
